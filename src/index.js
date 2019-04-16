c463ebef1305da2c8487bbfe0cf3803bfc782147
Alexander Polovnikov <alexpol25@yandex.ru>
2019-04-17 02:28:55 +0300

    Task-5

--- src/index.js
+++ src/index.js
@@ -1,45 +1,89 @@
 import { has, difference, intersection } from 'lodash/fp';
 import parse from './parsers';
 
-const genDiff = (firstFilePath, secondFilePath) => {
-  const firstFile = parse(firstFilePath);
-  const secondFile = parse(secondFilePath);
-  const firstKeys = Object.keys(firstFile);
-  const secondKeys = Object.keys(secondFile);
-  const removedKeys = difference(firstKeys, secondKeys).reduce(
-    (acc, key) => ({ ...acc, [`- ${key}`]: firstFile[key] }),
-    {},
-  );
-  const addedKeys = difference(secondKeys, firstKeys).reduce(
-    (acc, key) => ({ ...acc, [`+ ${key}`]: secondFile[key] }),
-    {},
-  );
-  const commonKeys = intersection(secondKeys, firstKeys).reduce((acc, key) => {
-    const noChangeVal =
-      has(key, firstFile) && firstFile[key] === secondFile[key];
-    return noChangeVal
-      ? {
-          ...acc,
-          [`  ${key}`]: firstFile[key],
-        }
-      : {
-          ...acc,
-          [`+ ${key}`]: secondFile[key],
-          [`- ${key}`]: firstFile[key],
-        };
-  }, {});
+const diffKeys = (before, after) => {
+  const beforeKeys = Object.keys(before);
+  const afterKeys = Object.keys(after);
+  const removedKeys = difference(beforeKeys, afterKeys).map(key => ({
+    key,
+    value: before[key],
+    type: 'removed',
+  }));
+  const addedKeys = difference(afterKeys, beforeKeys).map(key => ({
+    key,
+    value: after[key],
+    type: 'added',
+  }));
+  const commonKeys = intersection(afterKeys, beforeKeys).map(key => {
+    const noChangeVal = has(key, before) && before[key] === after[key];
+    const beforeHasChildren = typeof before[key] === 'object';
+    const afterHasChildren = typeof after[key] === 'object';
+    if (noChangeVal) {
+      return {
+        key,
+        type: 'equals',
+        value: before[key],
+      };
+    }
+    if (beforeHasChildren && afterHasChildren) {
+      return {
+        key,
+        type: 'changedChildren',
+        children: diffKeys(before[key], after[key]),
+      };
+    }
+    return {
+      key,
+      type: 'changed',
+      value: after[key],
+      before: before[key],
+    };
+  });
 
-  return JSON.stringify(
-    {
-      ...commonKeys,
-      ...addedKeys,
-      ...removedKeys,
-    },
-    null,
-    2,
-  )
+  return [...commonKeys, ...addedKeys, ...removedKeys];
+};
+
+const render = ast => {
+  const iter = tree => {
+    return tree.reduce((acc, item) => {
+      const { type, value, children, key, before } = item;
+
+      switch (type) {
+        case 'removed':
+          return { ...acc, [`- ${key}`]: value };
+        case 'added':
+          return { ...acc, [`+ ${key}`]: value };
+        case 'changed':
+          return {
+            ...acc,
+            [`+ ${key}`]: value,
+            [`- ${key}`]: before,
+          };
+        case 'changedChildren':
+          return {
+            ...acc,
+            [key]: iter(children),
+          };
+        default:
+          return {
+            ...acc,
+            [key]: value,
+          };
+      }
+    }, {});
+  };
+  return JSON.stringify(iter(ast), null, 2)
     .replace(/"([^"]+)":/g, '$1:')
     .replace(/: "([^"]+)"/g, ': $1')
     .replace(/,$/gm, '');
 };
+
+const genDiff = (firstFilePath, secondFilePath) => {
+  const firstFile = parse(firstFilePath);
+  const secondFile = parse(secondFilePath);
+  const diff = diffKeys(firstFile, secondFile);
+
+  return render(diff);
+};
+
 export default genDiff;
